bcb298ac7b016d2d2294048bd5c73b04647f63bf
Nicholas St. Germain <nick@cajun.pro>
2020-07-06 18:24:05 -0500

    build custom toJSON renderer

--- internal/handlers/env.go
+++ internal/handlers/env.go
@@ -2,16 +2,20 @@ package handlers
 
 import (
 	"bytes"
+	"encoding/json"
 	"net/http"
 
 	"github.com/sirupsen/logrus"
 
+	v1 "github.com/DirtyCajunRice/PeUD/api/v1"
 	"github.com/DirtyCajunRice/PeUD/internal/config"
 )
 
 type Env struct {
-	Log    *logrus.Logger
-	Config *config.Config
+	Log           *logrus.Entry
+	MiddlewareLog *logrus.Entry
+	Config        *config.Config
+	Build         *v1.Version
 }
 
 type Handler struct {
@@ -28,4 +32,24 @@ func arrayOrObject(data []byte) (isArray, isObject bool) {
 	isArray = len(x) > 0 && x[0] == '['
 	isObject = len(x) > 0 && x[0] == '{'
 	return
+}
+
+func (e *Env) toJSON(w http.ResponseWriter, r *http.Request, i interface{}, status ...int) {
+	log := e.Log.WithField("function", "tojson")
+	buffer := &bytes.Buffer{}
+	encoder := json.NewEncoder(buffer)
+	encoder.SetEscapeHTML(true)
+	if err := encoder.Encode(i); err != nil {
+		log.Error(err)
+		http.Error(w, "Could not marshal response to JSON", http.StatusInternalServerError)
+	}
+	if len(status) > 0 && status[0] != 0 {
+		w.WriteHeader(status[0])
+	} else {
+		w.WriteHeader(http.StatusOK)
+	}
+	if _, err := w.Write(buffer.Bytes()); err != nil {
+		log.Error(err)
+		http.Error(w, "Could not write http response", http.StatusInternalServerError)
+	}
 }
