e5d35526b3cb3e186e51251256868a034dee145b
Nicholas St. Germain <nick@cajun.pro>
2020-07-06 18:23:47 -0500

    add beginning struct for holding each api auth token

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,12 +3,18 @@ package config
 import "github.com/DirtyCajunRice/PeUD/internal/database"
 
 type Config struct {
-	APIServer *APIServer
-	Debug     bool
-	Database  *database.Database
+	APIServer      *APIServer
+	Debug          bool
+	Database       *database.Database
+	Authentication *Authentication
 }
 
 type APIServer struct {
 	Address string
 	Port    int
+}
+
+type Authentication struct {
+	PlexToken   string
+	TautulliKey string
 }
