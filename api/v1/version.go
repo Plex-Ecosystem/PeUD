9a4a10c6f8a648641ba4f17bed26b90825c626d5
Nicholas St. Germain <nick@cajun.pro>
2020-07-06 18:25:58 -0500

    rewrite version endpoint to use new json renderer

---
+++ api/v1/version.go
@@ -0,0 +1,6 @@
+package v1
+
+type Version struct {
+	Date    *string `json:"date"`
+	Version *string `json:"version"`
+}
--- internal/handlers/version.go
+++ internal/handlers/version.go
@@ -1,24 +1,11 @@
 package handlers
 
 import (
-	"encoding/json"
 	"net/http"
 )
 
-type VersionHandler struct {
-	Date    *string `json:"date"`
-	Version *string `json:"version"`
-}
-
-func NewVersionHandler(version *string, date *string) *VersionHandler {
-	return &VersionHandler{
-		Date:    date,
-		Version: version,
-	}
-}
-
 // GetVersion returns a JSON formatted VersionHandler struct
-func (v *VersionHandler) GetVersion(handlerEnv *Env, w http.ResponseWriter, r *http.Request) {
+func Version(Env *Env, w http.ResponseWriter, r *http.Request) {
 	/**
 	* @api {get} /version Get Version
 	* @apiName Version
@@ -49,18 +36,6 @@ func (v *VersionHandler) GetVersion(handlerEnv *Env, w http.ResponseWriter, r *h
 	*     }
 	* @apiSampleRequest /version
 	 */
-	log := handlerEnv.Log
-	b, err := json.Marshal(v)
-	if err != nil {
-		log.WithField("handler", "version").Error("could not marshal app version", err)
-		http.Error(w, "Could not get app version", http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	_, err = w.Write(b)
-	if err != nil {
-		log.WithField("handler", "version").Error("could not write response", err)
-		http.Error(w, "Could not write http response", http.StatusInternalServerError)
-	}
+	//log := Env.Log
+	Env.toJSON(w, r, Env.Build)
 }
