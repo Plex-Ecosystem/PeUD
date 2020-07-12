5bf06584e34b3203dcca9a0e43b41744759514f6
Nicholas St. Germain <nick@cajun.pro>
2020-07-11 22:44:00 -0500

    finish move of docs to public domain

--- internal/server/routers.go
+++ internal/server/routers.go
@@ -3,9 +3,6 @@ package server
 import (
 	"fmt"
 	"net/http"
-	"os"
-	"path/filepath"
-	"strings"
 	"time"
 
 	"github.com/go-chi/chi"
@@ -15,23 +12,6 @@ import (
 	"github.com/DirtyCajunRice/PeUD/internal/handlers"
 	peudMiddleware "github.com/DirtyCajunRice/PeUD/internal/middleware"
 )
-
-func fileServer(r chi.Router, p string, f http.FileSystem) {
-	if strings.ContainsAny(p, "{}*") {
-		panic("FileServer does not permit any URL parameters.")
-	}
-	if p != "/" && p[len(p)-1] != '/' {
-		r.Get(p, http.RedirectHandler(p+"/", 301).ServeHTTP)
-		p += "/"
-	}
-	p += "*"
-	r.Get(p, func(w http.ResponseWriter, r *http.Request) {
-		rc := chi.RouteContext(r.Context())
-		pp := strings.TrimSuffix(rc.RoutePattern(), "/*")
-		fs := http.StripPrefix(pp, http.FileServer(f))
-		fs.ServeHTTP(w, r)
-	})
-}
 
 func Start(Env *handlers.Env) {
 	log := Env.Log
@@ -46,23 +26,17 @@ func Start(Env *handlers.Env) {
 		middleware.RealIP,
 		peudMiddleware.NewMiddlewareLogger(Env.MiddlewareLog),
 		middleware.Compress(5),
+		middleware.RedirectSlashes,
 	)
-
-	//workDir, _ := os.Getwd()
-	//docs := http.Dir(filepath.Join(workDir, "docs/api"))
 	router.Route("/", func(r chi.Router) {
-		//fileServer(r, "/api/docs", docs)
-		r.With(middleware.RedirectSlashes).Route("/api", func(r chi.Router) {
-			r.Method(http.MethodGet, "/version", handlers.Handler{Env: Env, Handle: handlers.Version})
-			r.Route("/users", func(r chi.Router) {
-				r.Method(http.MethodGet, "/{plex,tautulli,organizr,ombi}", handlers.Handler{Env: Env, Handle: handlers.ListUsers})
-				r.Method(http.MethodGet, "/{plex,tautulli,organizr,ombi}/{id:[0-9]+}", handlers.Handler{Env: Env, Handle: handlers.GetUser})
-			})
-			r.Route("/sync", func(r chi.Router) {
-				r.Method(http.MethodPut, "/", handlers.Handler{Env: Env, Handle: handlers.Sync})
-			})
+		r.Method(http.MethodGet, "/version", handlers.Handler{Env: Env, Handle: handlers.Version})
+		r.Route("/users", func(r chi.Router) {
+			r.Method(http.MethodGet, "/{plex,tautulli,organizr,ombi}", handlers.Handler{Env: Env, Handle: handlers.ListUsers})
+			r.Method(http.MethodGet, "/{plex,tautulli,organizr,ombi}/{id:[0-9]+}", handlers.Handler{Env: Env, Handle: handlers.GetUser})
 		})
-
+		r.Route("/sync", func(r chi.Router) {
+			r.Method(http.MethodPut, "/", handlers.Handler{Env: Env, Handle: handlers.Sync})
+		})
 	})
 
 	config.Database.Init()
