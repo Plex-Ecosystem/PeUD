14c7426afecdd5b55a5436ac4667b4271bdbc9f7
Nicholas St. Germain <nick@cajun.pro>
2020-07-20 22:15:04 -0500

    first fixes to convertible boolean

--- api/v1/tautulli/users.go
+++ api/v1/tautulli/users.go
@@ -1,5 +1,7 @@
 package tautulli
 
+import "github.com/DirtyCajunRice/go-utility/types"
+
 type UserResponse struct {
 	BaseResponse
 	Data User `json:"data"`
@@ -9,25 +11,25 @@ type UsersResponse struct {
 	Data []User `json:"data"`
 }
 type User struct {
-	RowID           int    `json:"row_id"`
-	UserID          int    `json:"user_id"`
-	Username        string `json:"username"`
-	FriendlyName    string `json:"friendly_name"`
-	Thumb           string `json:"thumb"`
-	Email           string `json:"email"`
-	IsActive        int    `json:"is_active"`
-	IsAdmin         int    `json:"is_admin"`
-	IsHomeUser      int    `json:"is_home_user"`
-	IsAllowSync     int    `json:"is_allow_sync"`
-	IsRestricted    int    `json:"is_restricted"`
-	DoNotify        int    `json:"do_notify"`
-	KeepHistory     int    `json:"keep_history"`
-	AllowGuest      int    `json:"allow_guest"`
-	ServerToken     string `json:"server_token"`
-	SharedLibraries string `json:"shared_libraries"`
-	FilterAll       string `json:"filter_all"`
-	FilterMovies    string `json:"filter_movies"`
-	FilterTv        string `json:"filter_tv"`
-	FilterMusic     string `json:"filter_music"`
-	FilterPhotos    string `json:"filter_photos"`
+	RowID           int                      `json:"row_id"`
+	UserID          int                      `json:"user_id"`
+	Username        string                   `json:"username"`
+	FriendlyName    string                   `json:"friendly_name"`
+	Thumb           string                   `json:"thumb"`
+	Email           string                   `json:"email"`
+	IsActive        types.ConvertibleBoolean `json:"is_active"`
+	IsAdmin         types.ConvertibleBoolean `json:"is_admin"`
+	IsHomeUser      types.ConvertibleBoolean `json:"is_home_user"`
+	IsAllowSync     types.ConvertibleBoolean `json:"is_allow_sync"`
+	IsRestricted    types.ConvertibleBoolean `json:"is_restricted"`
+	DoNotify        types.ConvertibleBoolean `json:"do_notify"`
+	KeepHistory     types.ConvertibleBoolean `json:"keep_history"`
+	AllowGuest      types.ConvertibleBoolean `json:"allow_guest"`
+	ServerToken     string                   `json:"server_token"`
+	SharedLibraries string                   `json:"shared_libraries"`
+	FilterAll       string                   `json:"filter_all"`
+	FilterMovies    string                   `json:"filter_movies"`
+	FilterTv        string                   `json:"filter_tv"`
+	FilterMusic     string                   `json:"filter_music"`
+	FilterPhotos    string                   `json:"filter_photos"`
 }
